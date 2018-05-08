21d2d5c992979fb05b28bf8e3ce5f902763cb875
David <waywardblues@gmail.com>
2018-05-08 18:28:11 +0700

    Init commit

--- app/app.jsx
+++ app/app.jsx
@@ -1,6 +1,7 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 var {Route, Router, IndexRoute, hashHistory} = require('react-router');
+var TodoApp = require('TodoApp');
 
 
 
@@ -11,6 +12,8 @@ $(document).foundation();
 require('style!css!sass!applicationStyles')
 
 ReactDOM.render(
-  <p>Boilerplate 3 Project</p>,
+  <div>
+    <TodoApp/>
+  </div>,
   document.getElementById('app')
 );
